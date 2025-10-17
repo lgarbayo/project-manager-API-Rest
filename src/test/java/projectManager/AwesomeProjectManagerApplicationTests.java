b537990eaa70191798f0cf4d77f55dc6c274939b
Luis Garbayo <lugarbayo@gmail.com>
2025-10-17 11:29:26 +0200

    feat: first commit

---
+++ src/test/java/projectManager/AwesomeProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package projectManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
